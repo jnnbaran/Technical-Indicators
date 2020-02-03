537deb6146ccaa7387f39788811a4507ea15093a
jnnbaran <jnnbaran@gmail.com>
2020-02-03 22:47:02 +0100

    Fix missing methods from adx

--- src/main/java/com/example/adx/ADX.java
+++ src/main/java/com/example/adx/ADX.java
@@ -1,6 +1,7 @@
 package com.example.adx;
 
 import com.example.adx.exc.NotEnoughDataForGivenRange;
+import com.example.data.Reader;
 import com.example.data.Record;
 
 import java.math.BigDecimal;
@@ -15,12 +16,12 @@ public class ADX {
     public static BigDecimal countAverageDirectionalIndex(List<Record> allRecords, int range, int period) {
         List<List<Record>> recordsWithHistory = new ArrayList<>();
         List<BigDecimal> directionalIndexes = new ArrayList<>();
-        List<Record> latestRecords = getLatestNRecordsList(allRecords, range);
+        List<Record> latestRecords = Reader.getLatestNRecordsList(allRecords, range);
 
         latestRecords.stream()
                 .filter(record -> allRecords.size() - allRecords.indexOf(record) >= period + 1)
                 .forEach(
-                currentRecord -> recordsWithHistory.add(getRecordsWithHistory(allRecords, currentRecord, period))
+                currentRecord -> recordsWithHistory.add(Reader.getRecordsWithHistory(allRecords, currentRecord, period))
         );
 
         for(List<Record> records : recordsWithHistory) {
@@ -34,22 +35,6 @@ public class ADX {
                 .reduce(BigDecimal::add)
                 .orElseThrow(NotEnoughDataForGivenRange::new)
                 .divide(new BigDecimal(directionalIndexes.size()), BigDecimal.ROUND_HALF_EVEN);
-    }
-
-    public static List<Record> getLatestNRecordsList(List<Record> list, int range) {
-        List<Record> records = list.stream()
-                .sorted(Comparator.comparing(Record::getDate))
-                .filter(record -> list.indexOf(record) < range)
-                .collect(Collectors.toList());
-        Collections.reverse(records);
-        return records;
-    }
-
-    public static List<Record> getRecordsWithHistory(List<Record> list, Record currentRecord, int periods) {
-        return list.stream().filter(
-                record -> list.indexOf(record) - list.indexOf(currentRecord) <= periods &&
-                          list.indexOf(record) >= list.indexOf(currentRecord)
-        ).collect(Collectors.toList());
     }
 
     public static BigDecimal countPositiveDirectionalIndicator(List<Record> records) {
